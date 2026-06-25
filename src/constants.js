560a69162dbb604c78a6131fe5990672cda6d12e
zadshogar <cars@stormepic.com>
2026-06-25 08:30:02 -0500

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,5 @@ export const C = {
   bg:'#0c0c0e', surface:'#161618', surface2:'#1e1e22', border:'#2a2a32',
   accent:'#e8a020', green:'#3de08a', red:'#e03a3a', text:'#eeeef2', muted:'#80809a',
 };
+
 export const STORAGE_KEYS = { parts:'zp_parts', txns:'zp_txns' };
