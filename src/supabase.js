4488c8503543bc32cc43f2669dcabe41d582f2e7
zadshogar <cars@stormepic.com>
2026-06-25 08:27:24 -0500

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL      = 'https://macjedqrgyposvqgwdof.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hY2plZHFyZ3lwb3N2cWd3ZG9mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4NDU1MDksImV4cCI6MjA4ODQyMTUwOX0.p-yKt8gzK6NMAHTCE-SNCz9M--kFfi5gNicj4ekNIoY';
+const SUPABASE_URL      = 'https://puphzwdlzqgbhmogiern.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_aMzGc8o0pu88iBMzaveKmg_KDwgH9a7';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
